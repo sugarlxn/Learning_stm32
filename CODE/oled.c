899c896acc41c9126bfcbd90cf52a1f6a272c71f
sugarlxn <1933650668@qq.com>
2021-11-08 17:36:07 +0800

    update the code of timer

--- CODE/oled.c
+++ CODE/oled.c
@@ -42,16 +42,17 @@ void I2C_Configuration(void){
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_OD;//I2C必须开漏输出
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-	I2C_DeInit(I2C2);//使用I2C1
+	I2C_DeInit(I2C2);//使用I2C2
 	I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
 	I2C_InitStructure.I2C_DutyCycle = I2C_DutyCycle_2;
 	I2C_InitStructure.I2C_OwnAddress1 = 0x30;//主机的I2C地址,随便写的
 	I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
 	I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
 	I2C_InitStructure.I2C_ClockSpeed = 400000;//400K
-
-	I2C_Cmd(I2C2, ENABLE);
+	
 	I2C_Init(I2C2, &I2C_InitStructure);
+	I2C_Cmd(I2C2, ENABLE);
+	
 	
 }
 
